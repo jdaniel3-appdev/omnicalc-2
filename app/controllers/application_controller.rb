585e38332e8b6db1338cc17c185a3c8d486bd449
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-02-01 17:09:36 +0000

    Commit 1 Addition works, rails grade recognizes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  def blank_add_form
+    render({ :template => "/add.html.erb"})
+  end
+
+  def add_results
+    
+    @nr1  = params.fetch("user_1").to_f
+    @nr2  = params.fetch("user_2").to_f
+    @sum = @nr1 + @nr2
+
+    render({ :template => "/addition_results.html.erb"})
+  end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/add", { :controller => "application", :action => "blank_add_form"})
+  get("/wizard_add", { :controller => "application", :action => "add_results"})
+
 end
