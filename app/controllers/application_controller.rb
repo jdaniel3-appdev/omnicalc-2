796374717d2ccd21156a32e8661ddfbad41c5df8
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-02-02 02:16:40 +0000

    Commit 2 Add, Subtract, Multiply, Divide all working, rails grade 100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,43 @@ class ApplicationController < ActionController::Base
     render({ :template => "/addition_results.html.erb"})
   end
 
+  def blank_subtract_form
+    render({ :template => "/subtract.html.erb"})
+  end
   
+  def subtract_results
+    
+    @nr1  = params.fetch("user_1").to_f
+    @nr2  = params.fetch("user_2").to_f
+    @difference = @nr2 - @nr1
+
+    render({ :template => "/subtraction_results.html.erb"})
+  end
+
+  def blank_multiply_form
+    render({ :template => "/multiply.html.erb"})
+  end
+  
+  def multiply_results
+    
+    @nr1  = params.fetch("user_1").to_f
+    @nr2  = params.fetch("user_2").to_f
+    @product = @nr2 * @nr1
+
+    render({ :template => "/multiplication_results.html.erb"})
+  end
+
+  def blank_divide_form
+    render({ :template => "/divide.html.erb"})
+  end
+  
+  def divide_results
+    
+    @nr1  = params.fetch("user_1").to_f.round(9)
+    @nr2  = params.fetch("user_2").to_f.round(9)
+    @quotient = @nr1 / @nr2
+
+    render({ :template => "/division_results.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
   get("/add", { :controller => "application", :action => "blank_add_form"})
   get("/wizard_add", { :controller => "application", :action => "add_results"})
 
+  get("/subtract", { :controller => "application", :action => "blank_subtract_form"})
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_results"})
+
+  get("/multiply", { :controller => "application", :action => "blank_multiply_form"})
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_results"})
+
+  get("/divide", { :controller => "application", :action => "blank_divide_form"})
+  get("/wizard_divide", { :controller => "application", :action => "divide_results"})
+
+
 end
